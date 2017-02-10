9b06bd6bdd3b1afba8fe793ed40950c3fd160c90
Bart Oldeman <bart.oldeman@calculquebec.ca>
2017-02-10 15:40:02 +0000

    rpm: needs bfd.h from binutils, and a fix (taken from Arch Linux)

--- pkgs/tools/package-management/rpm/default.nix
+++ pkgs/tools/package-management/rpm/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, cpio, zlib, bzip2, file, elfutils, libarchive, nspr, nss, popt, db, xz, python, lua, pkgconfig, autoreconfHook }:
+{ stdenv, fetchurl, cpio, zlib, bzip2, file, elfutils, libarchive, nspr, nss, popt, db, xz, python, lua, pkgconfig, autoreconfHook, binutils }:
 
 stdenv.mkDerivation rec {
   name = "rpm-4.13.0";
@@ -8,10 +8,12 @@ stdenv.mkDerivation rec {
     sha256 = "221166b61584721a8ca979d7d8576078a5dadaf09a44208f69cc1b353240ba1b";
   };
 
-  buildInputs = [ cpio zlib bzip2 file libarchive nspr nss db xz python lua pkgconfig autoreconfHook ];
+  buildInputs = [ cpio zlib bzip2 file libarchive nspr nss db xz python lua pkgconfig autoreconfHook binutils ];
 
   # rpm/rpmlib.h includes popt.h, and then the pkg-config file mentions these as linkage requirements
   propagatedBuildInputs = [ popt elfutils nss db bzip2 libarchive ];
+
+  patches = [ ./bfdfix.patch ];
 
   NIX_CFLAGS_COMPILE = "-I${nspr.dev}/include/nspr -I${nss.dev}/include/nss";
 
