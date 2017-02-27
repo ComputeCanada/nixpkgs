0a77bd4bdcd34c994bc28fc691d77e279d820d37
Bart Oldeman <bart.oldeman@calculquebec.ca>
2017-02-27 16:57:22 +0000

    Fix URL for OPA-PSM2

--- pkgs/development/libraries/opa-psm2/default.nix
+++ pkgs/development/libraries/opa-psm2/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "opa-psm2-10.2.42";
 
   src = fetchurl {
-    url = " https://github.com/01org/opa-psm2/archive/PSM2_10_2_42.tar.gz";
+    url = "https://github.com/01org/opa-psm2/archive/PSM2_10.2-42.tar.gz";
     sha256 = "039445a973d3f222a70122d6099aa5ae7a912d92ddcb123110da5dd85f39a5d9";
   };
 
