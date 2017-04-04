a429c78e33bc677caf1c42f3cc0daf3495421004
Bart Oldeman <bart.oldeman@calculquebec.ca>
2017-04-04 14:10:36 +0000

    graphicsmagick: correct url for patches

--- pkgs/applications/graphics/graphicsmagick/default.nix
+++ pkgs/applications/graphics/graphicsmagick/default.nix
@@ -15,31 +15,31 @@ stdenv.mkDerivation {
   patches = [
     ./disable-popen.patch
     (fetchpatch {
-      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-5/debian/patches/CVE-2016-7996_CVE-2016-7997.patch";
+      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-8/debian/patches/CVE-2016-7996_CVE-2016-7997.patch";
       sha256 = "0xsby2z8n7cnnln7szjznq7iaabq323wymvdjra59yb41aix74r2";
     })
     (fetchpatch {
-      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-5/debian/patches/CVE-2016-7800_part1.patch";
+      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-8/debian/patches/CVE-2016-7800_part1.patch";
       sha256 = "02s0x9bkbnm5wrd0d2x9ld4d9z5xqpfk310lyylyr5zlnhqxmwgn";
     })
     (fetchpatch {
-      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-5/debian/patches/CVE-2016-7800_part2.patch";
+      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-8/debian/patches/CVE-2016-7800_part2.patch";
       sha256 = "1h4xv3i1aq5avsd584rwa5sa7ca8f7w9ggmh7j2llqq5kymwsv5f";
     })
     (fetchpatch {
-      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-5/debian/patches/CVE-2016-8682.patch";
+      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-8/debian/patches/CVE-2016-8682.patch";
       sha256 = "1wfirw2yi5y72657kvnbgjs0f9b3rs9nvk8gjbwhb9a03z9ws0y5";
     })
     (fetchpatch {
-      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-5/debian/patches/CVE-2016-8683.patch";
+      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-8/debian/patches/CVE-2016-8683.patch";
       sha256 = "102252zb34nj6alk1nhh1wbn3apd2v9rzk7clmm237332yj72vif";
     })
     (fetchpatch {
-      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-5/debian/patches/CVE-2016-8684.patch";
+      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-8/debian/patches/CVE-2016-8684.patch";
       sha256 = "1p36gpz904wnmbz1n64x4pdpg8lp9zs3gx0awklxqdvgl8m82vvy";
     })
     (fetchpatch {
-      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-7/debian/patches/CVE-2016-9830.patch";
+      url = "https://sources.debian.net/data/main/g/graphicsmagick/1.3.25-8/debian/patches/CVE-2016-9830.patch";
       sha256 = "0qh15sd7nx7vf9sld4453iml951bwsx2fx84hxc7plhds2k3gjpa";
     })
   ];
