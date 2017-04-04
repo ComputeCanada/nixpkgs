56d8eca89adff074d78639e88ff16c43d667ab50
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2017-04-04 16:57:26 +0000

    fixed store paths in the environment

--- pkgs/tools/misc/lmod/default.nix
+++ pkgs/tools/misc/lmod/default.nix
@@ -12,7 +12,12 @@ stdenv.mkDerivation rec {
   buildInputs = [ lua tcl perl rsync procps ];
   propagatedBuildInputs = [ luaposix luafilesystem ];
   preConfigure = '' makeFlags="PREFIX=$out" '';
-  configureFlags = [ "--with-duplicatePaths=yes --with-caseIndependentSorting=yes --with-redirect=yes --with-module-root-path=/cvmfs/soft.cc/nix/1/easybuild/generic/modules" ];
+  configureFlags = [ "--with-duplicatePaths=yes --with-caseIndependentSorting=yes --with-redirect=yes --with-module-root-path=/cvmfs/soft.computecanada.ca/easybuild/modules" ];
+
+  # replace nix-store paths in the environment with nix-profile paths to allow easy upgrade
+  postInstall = ''
+    sed -i -e "s;/cvmfs/soft.computecanada.ca/nix/store/[^/]*;/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09;g" $out/lmod/lmod/init/* 
+  '';
 
   LUA_PATH="${luaposix}/share/lua/5.2/?.lua;${luaposix}/share/lua/5.2/?/init.lua;;";
   LUA_CPATH="${luafilesystem}/lib/lua/5.2/?.so;${luaposix}/lib/lua/5.2/?.so;;";
