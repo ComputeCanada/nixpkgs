c65d44a771097cab50ec1c4e7b50f924e3b40117
José Romildo Malaquias <malaquias@gmail.com>
2017-12-31 13:03:33 -0200

    mate-system-monitor: init at 1.18.1

--- pkgs/desktops/mate/default.nix
+++ pkgs/desktops/mate/default.nix
@@ -30,6 +30,7 @@ let
     mate-session-manager = callPackage ./mate-session-manager { };
     mate-settings-daemon = callPackage ./mate-settings-daemon { };
     mate-screensaver = callPackage ./mate-screensaver { };
+    mate-system-monitor = callPackage ./mate-system-monitor { };
     mate-terminal = callPackage ./mate-terminal { };
     mate-themes = callPackage ./mate-themes { };
     mate-user-guide = callPackage ./mate-user-guide { };
@@ -65,6 +66,7 @@ let
       mate-media
       mate-power-manager
       mate-screensaver
+      mate-system-monitor
       mate-terminal
       mate-user-guide
       pluma
