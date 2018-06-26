96ab596961da024133c4c2c4ac8fdb023067ae50
Bart Oldeman <bart.oldeman@calculquebec.ca>
2018-06-26 18:59:25 +0000

    Fix typo for nss-symlinks for sss.

--- pkgs/os-specific/linux/nss-symlinks/default.nix
+++ pkgs/os-specific/linux/nss-symlinks/default.nix
@@ -13,6 +13,6 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p "$out"/lib
     ln -s /lib64/libnss_ldap.so.2 "$out"/lib
-    ln -s /lib64/libnss_nss.so.2 "$out"/lib
+    ln -s /lib64/libnss_sss.so.2 "$out"/lib
   '';
 }
