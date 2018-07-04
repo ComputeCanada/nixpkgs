9e1e4fba5191b60e85f44fbee8901c84bb7dbb6d
Bart Oldeman <bart.oldeman@calculquebec.ca>
2018-07-04 18:02:33 +0000

    szip: update url.

--- pkgs/development/libraries/szip/default.nix
+++ pkgs/development/libraries/szip/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation {
   name = "szip-2.1";
   src = fetchurl {
-    url = ftp://ftp.hdfgroup.org/lib-external/szip/2.1/src/szip-2.1.tar.gz;
+    url = https://support.hdfgroup.org/ftp/lib-external/szip/previous/2.1/src/szip-2.1.tar.gz;
     sha256 = "1vym7r4by02m0yqj10023xyps5b21ryymnxb4nb2gs32arfxj5m8";
   };
 
