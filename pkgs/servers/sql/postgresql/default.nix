efc9e765b641e3e760b90dab8df4291a5edf693e
aroudgar <aroudgar@sfu.ca>
2018-10-01 14:11:13 -0700

    Update default.nix

--- pkgs/servers/sql/postgresql/default.nix
+++ pkgs/servers/sql/postgresql/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, glibc, fetchurl, zlib, readline, libossp_uuid, openssl, makeWrapper, pam openldap }:
+{ lib, stdenv, glibc, fetchurl, zlib, readline, libossp_uuid, openssl, makeWrapper, pam, openldap }:
 
 let
 
