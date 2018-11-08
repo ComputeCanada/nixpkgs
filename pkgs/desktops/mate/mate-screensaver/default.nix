c6f79790e1a836befe9471638e5e9823c965cc4e
Bart Oldeman <bart.oldeman@calculquebec.ca>
2018-11-08 15:04:42 +0000

    Revert "MATE: Use password helper for the screensaver."
    This reverts commit ab70a759c15b8955d5e9cf8a99ecf49979850cec.

--- pkgs/desktops/mate/mate-screensaver/default.nix
+++ pkgs/desktops/mate/mate-screensaver/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, intltool, gtk3, dbus_glib, libXScrnSaver, libnotify, mate-desktop, mate-menus, wrapGAppsHook }:
+{ stdenv, fetchurl, pkgconfig, intltool, gtk3, dbus_glib, libXScrnSaver, libnotify, pam, systemd, mate-desktop, mate-menus, wrapGAppsHook }:
 
 stdenv.mkDerivation rec {
   name = "mate-screensaver-${version}";
@@ -22,11 +22,13 @@ stdenv.mkDerivation rec {
     dbus_glib
     libXScrnSaver
     libnotify
+    pam
+    systemd
     mate-desktop
     mate-menus
   ];
 
-  configureFlags = "--without-console-kit --enable-authentication-scheme=helper --with-passwd-helper=/sbin/unix_chkpwd";
+  configureFlags = "--without-console-kit";
 
   makeFlags = "DBUS_SESSION_SERVICE_DIR=$(out)/etc";
 
