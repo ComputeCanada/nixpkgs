4f26b6651ecc576e0e9ee01021346b319c8a35cc
Bart Oldeman <bart.oldeman@calculquebec.ca>
2018-12-04 14:30:35 +0000

    ucx: update to 1.5.0 from hpcx 2.3

--- pkgs/development/libraries/ucx/default.nix
+++ pkgs/development/libraries/ucx/default.nix
@@ -1,19 +1,21 @@
-{ stdenv, fetchurl, pkgconfig, graphviz, numactl, binutils, libiberty,
+{ stdenv, fetchurl, autoconf, automake, git, libtool, gettext, pkgconfig, graphviz, numactl, binutils, libiberty,
   rdma-core, zlib, knem, doxygen, perl, texlive }:
 
 stdenv.mkDerivation rec {
-  version = "1.4.0";
+  version = "1.5.0-hpcx-v2.3";
   name = "ucx-${version}";
 
   src = fetchurl {
-    url = "https://github.com/openucx/ucx/releases/download/v${version}/${name}.tar.gz";
-    sha256 = "99891a98476bcadc6ac4ef9c9f083bc6ffb188a96b3c3bc89c8bbca64de2c76e";
+    #url = "https://github.com/openucx/ucx/releases/download/v${version}/${name}.tar.gz";
+    url = "https://github.com/openucx/ucx/archive/hpcx-v2.3.tar.gz";
+    sha256 = "44bdd30d54cbe30d98775f9bde490355f78048460fef0259feeb1a405fc1a248";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ autoconf automake git libtool gettext pkgconfig ];
   buildInputs = [ numactl graphviz binutils libiberty rdma-core zlib knem
                   doxygen perl texlive.combined.scheme-basic ];
 
+  preConfigure = "./autogen.sh";
   configureScript = "./contrib/configure-release";
   configureFlags = [ "--with-verbs=${rdma-core}" "--with-rdmacm=${rdma-core}" "--with-knem=${knem}" ];
 
