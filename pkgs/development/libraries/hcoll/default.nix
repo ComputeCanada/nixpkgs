6876c202312b407663a4ec24402237cccfe44f52
Bart Oldeman <bart.oldeman@calculquebec.ca>
2018-12-05 01:57:27 +0000

    hcoll: fix rpath for real

--- pkgs/development/libraries/hcoll/default.nix
+++ pkgs/development/libraries/hcoll/default.nix
@@ -23,15 +23,8 @@ stdenv.mkDerivation rec {
     cd hcoll
     mkdir -p $out/bin $out/lib/hcoll $out/etc/hcoll $out/share/hcoll $out/share/doc/hcoll
     install -m755 bin/* $out/bin
-    for i in $out/bin/*; do
-      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $i
-      patchelf --set-rpath $out/lib:${rdma-core}/lib:${ucx}/lib $i
-    done
     install -m755 lib/lib*.a lib/lib*.so.*.* $out/lib
     install -m755 lib/hcoll/* $out/lib/hcoll
-    for i in $out/lib/lib*.so.*.*; do
-      patchelf --set-rpath $out/lib:${rdma-core}/lib:${ucx}/lib $i
-    done
     find include/ -type f -exec install -Dm 755 "{}" $out/"{}" \;
     install -m644 etc/* $out/etc/hcoll
     install -m644 share/hcoll/* $out/share/hcoll
@@ -43,6 +36,17 @@ stdenv.mkDerivation rec {
     ln -s libhcoll.so.*.* libhcoll.so
     ln -s libocoms.so.*.* libocoms.so.0
     ln -s libocoms.so.*.* libocoms.so  
+  '';
+
+
+  postFixup = ''
+    for i in $out/bin/*; do
+      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $i
+      patchelf --set-rpath $out/lib:${rdma-core}/lib:${ucx}/lib $i
+    done
+    for i in $out/lib/lib*.so.*.*; do
+      patchelf --set-rpath $out/lib:${rdma-core}/lib:${ucx}/lib $i
+    done
   '';
 
   dontStrip = true;
