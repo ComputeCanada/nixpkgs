d2c125845c8e1705995e8a4c8cfb9b2b2631ff82
Bart Oldeman <bart.oldeman@calculquebec.ca>
2019-03-04 20:22:50 +0000

    Update UCX to 1.5.0 and apply patch to avoid DC misdetection.

--- pkgs/development/libraries/ucx/default.nix
+++ pkgs/development/libraries/ucx/default.nix
@@ -2,20 +2,20 @@
   rdma-core, zlib, knem, doxygen, perl, texlive }:
 
 stdenv.mkDerivation rec {
-  version = "1.5.0-hpcx-v2.3";
+  version = "1.5.0";
   name = "ucx-${version}";
 
   src = fetchurl {
-    #url = "https://github.com/openucx/ucx/releases/download/v${version}/${name}.tar.gz";
-    url = "https://github.com/openucx/ucx/archive/hpcx-v2.3.tar.gz";
-    sha256 = "44bdd30d54cbe30d98775f9bde490355f78048460fef0259feeb1a405fc1a248";
+    url = "https://github.com/openucx/ucx/releases/download/v${version}/${name}.tar.gz";
+    sha256 = "0n3lgm5rxj4drp7hs4l3dz2mhvi00xf41fy8n6wypbs0azxf9xl4";
   };
+  patches = [./0001-UCT-IB-MLX5-Fasten-DC-support-check.patch];
 
   nativeBuildInputs = [ autoconf automake git libtool gettext pkgconfig ];
   buildInputs = [ numactl graphviz binutils libiberty rdma-core zlib knem
                   doxygen perl texlive.combined.scheme-basic ];
 
-  preConfigure = "./autogen.sh";
+  #preConfigure = "./autogen.sh";
   configureScript = "./contrib/configure-release";
   configureFlags = [ "--with-verbs=${rdma-core}" "--with-rdmacm=${rdma-core}" "--with-knem=${knem}"
                      "--disable-optimizations" "--enable-mt"];
