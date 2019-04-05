cbadfcdcf1fa81c151b82b2537a6539ccff5a0c8
Bart Oldeman <bart.oldeman@calculquebec.ca>
2019-04-05 16:26:57 +0000

    Update gcc symlinks to include gcc 8.

--- pkgs/development/compilers/gcc-symlinks/default.nix
+++ pkgs/development/compilers/gcc-symlinks/default.nix
@@ -1,8 +1,8 @@
-{stdenv, gfortran48, gfortran5, gfortran6, gfortran7}:
+{stdenv, gfortran48, gfortran5, gfortran6, gfortran7, gfortran8}:
 
 let
   name = "gccruntime-${version}";
-  version = "1.0.0";
+  version = "1.0.1";
 
 in
 stdenv.mkDerivation {
@@ -22,6 +22,9 @@ stdenv.mkDerivation {
       ln -sf $i "$out"/lib
     done 
     for i in ${gfortran7.cc.lib}/lib/lib*.so.*; do
+      ln -sf $i "$out"/lib
+    done
+    for i in ${gfortran8.cc.lib}/lib/lib*.so.*; do
       ln -sf $i "$out"/lib
     done
     ln -s ${gfortran5.cc}/bin "$out"/bin
