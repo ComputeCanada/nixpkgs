2c71e0a3217c025355eb3899993e54a0ca83003c
Bart Oldeman <bart.oldeman@calculquebec.ca>
2019-09-27 17:36:03 +0000

    Update glxinfo/glxgears to use libGL (libglvnd)

--- pkgs/tools/graphics/glxinfo/default.nix
+++ pkgs/tools/graphics/glxinfo/default.nix
@@ -10,19 +10,20 @@ stdenv.mkDerivation {
     sha256 = "1vqb7s5m3fcg2csbiz45mha1pys2xx6rhw94fcyvapqdpm5iawy1";
   };
 
-  buildInputs = [ xlibsWrapper mesa_noglu ];
+  buildInputs = [ xlibsWrapper libGL ];
 
   configurePhase = "true";
 
   buildPhase = "
-    cd src/xdemos
-    gcc glxinfo.c glinfo_common.c -o glxinfo -lGL -lX11
-    gcc glxgears.c -o glxgears -lGL -lX11
+    export NIX_LDFLAGS=\"$NIX_LDFLAGS -L${libGL}/lib\"
+    $CC src/xdemos/{glxinfo.c,glinfo_common.c} -o glxinfo -lGL -lX11
+    $CC src/xdemos/glxgears.c -o glxgears -lGL -lX11 -lm
+    $CC src/egl/opengles2/es2_info.c -o es2_info -lEGL -lGLESv2 -lX11
+    $CC src/egl/opengles2/es2gears.c src/egl/eglut/{eglut.c,eglut_x11.c} -o es2gears -Isrc/egl/eglut -lEGL -lGLESv2 -lX11 -lm
   ";
 
   installPhase = "
-    mkdir -p $out/bin
-    cp glxinfo glxgears $out/bin/
+    install -Dm 555 -t $out/bin glx{info,gears} es2{_info,gears}
   ";
 
   meta = {
