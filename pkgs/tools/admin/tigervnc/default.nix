ed6217dfd03055b1f0174398e24ca8fb2fe123af
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2020-04-28 13:23:47 +0000

    bumped tigervnc to 1.10.1

--- pkgs/tools/admin/tigervnc/default.nix
+++ pkgs/tools/admin/tigervnc/default.nix
@@ -7,14 +7,14 @@
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
-  version = "1.10.0";
+  version = "1.10.1";
   name = "tigervnc-${version}";
 
   src = fetchFromGitHub {
     owner = "TigerVNC";
     repo = "tigervnc";
-    sha256 = "0l0x7cq65wv9n93r952qsikwzcls1sq3r32mx0c4wg19dha0x1m4";
-    rev = "v1.10.0";
+    sha256 = "001n189d2f3psn7nxgl8188ml6f7jbk26cxn2835y3mnlk5lmhgr";
+    rev = "v1.10.1";
   };
 
   inherit fontDirectories;
