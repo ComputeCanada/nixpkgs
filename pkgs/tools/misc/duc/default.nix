bc7ed50ad426ad73fe3c5f7cc7df403841b682b2
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2021-01-12 21:33:38 +0000

    updated duc recipe to 1.4.4 and included sqlite3

--- pkgs/tools/misc/duc/default.nix
+++ pkgs/tools/misc/duc/default.nix
@@ -1,17 +1,19 @@
-{ stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, tokyocabinet, cairo, pango, ncurses }:
+{ stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, tokyocabinet, cairo, pango, ncurses, sqlite, pythonPackages }:
 
 stdenv.mkDerivation rec {
   name = "duc-${version}";
-  version = "1.4.1";
+  version = "1.4.4";
 
   src = fetchFromGitHub {
     owner = "zevv";
     repo = "duc";
     rev = "${version}";
-    sha256 = "0rnar2zacsb9rvdmp1a62xixhy69s5vh0nwgrklqxhb19qkzhdp7";
+    sha256 = "1i7ry25xzy027g6ysv6qlf09ax04q4vy0kikl8h0aq5jbxsl9q52";
   };
 
-  buildInputs = [ autoreconfHook pkgconfig tokyocabinet cairo pango ncurses ];
+  buildInputs = [ autoreconfHook pkgconfig tokyocabinet cairo pango ncurses sqlite ]
+                 ++ (with pythonPackages; [ sqlite3 ]);
+  configureFlags = [ "--with-db-backend=sqlite3" ];
 
   meta = with stdenv.lib; {
     homepage = http://duc.zevv.nl/;
